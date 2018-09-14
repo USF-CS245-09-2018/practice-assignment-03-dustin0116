e825442a0e7dadd081f39af2f09546962e02c9d6
Dustin Hsiang <dustin0116@gmail.com>
2018-09-13 23:06:22 -0700

    Finished.

--- Practice03Test.java
+++ Practice03Test.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Arrays;
 
 
 public class Practice03Test {
@@ -52,13 +53,26 @@ public class Practice03Test {
 	}
 
 
-	public int find_min_iterative () {
-		// TODO: Fill in this iterative function.
+	public int find_min_iterative (double[] arr) {
+		int min = 0; // Initialize minimum as the first element.
+		for (int i = 0; i<arr.length; i++) { // Traverse through array seeing if there are smaller elements than initial minimum.
+			if (arr[min] > arr[i]) { // If current minimum is smaller than another element, set the element as the minimum.
+				min = i;
+			}
+		}
+		return min;
 	}
 
 
-	public int find_min_recursive () {
-		// TODO: Fill in this recursive function.
+	public int find_min_recursive (double[] arr) {
+		if (arr.length == 1) { // Base case: return the first index if array has only one element.
+			return 0;
+		}
+		if (arr[0] < arr[arr.length - 1]) { // Traversing array from right comparing to left.
+			return find_min_iterative(Arrays.copyOfRange(arr, 0, arr.length-1));
+		} else { // Traversing array from left comparing right.
+			return 1 + find_min_iterative(Arrays.copyOfRange(arr, 1, arr.length));
+		}
 	}
 
 
@@ -67,8 +81,8 @@ public class Practice03Test {
 	 * ... and prints them both.
 	 */
 	public void print_min() {
-		System.out.println("Iteratively determined min at index " + find_min_iterative());
-		System.out.println("Recursively determined min at index " + find_min_recursive());
+		System.out.println("Iteratively determined min at index " + find_min_iterative(arr));
+		System.out.println("Recursively determined min at index " + find_min_recursive(arr));
 	}
 
 
